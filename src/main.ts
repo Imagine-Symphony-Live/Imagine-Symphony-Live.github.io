9891d9547262c889df8c0a737177fdfd7df95024
Dustin Woods <pyrozzcom@gmail.com>
2020-01-07 12:00:04 -0800

    cleanup main, put oveworld in function

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { Application, Container } from 'pixi.js';
 import { BioElement } from './bio-element';
 import { WORLD_WIDTH, WORLD_HEIGHT } from './constants';
 import TWEEN from '@tweenjs/tween.js';
-import { NucleusElement } from './nucleus-element';
 import { Toolbar } from './toolbar';
 import { FancyViewport } from './fancy-viewport';
 import { Instrument } from './types/instruments';
@@ -51,10 +50,11 @@ window.onload = async () => {
 
   await Promise.all([
     loadFonts(),
-    sounds.loadSounds(),
+    //sounds.loadSounds(),
   ]);
 
   function instrumentSoundActivate(name: Instrument) {
+    return;//disabled
     switch (name) {
       case "Violin":
         return sounds.activateTrack(track12);
@@ -100,7 +100,116 @@ window.onload = async () => {
     }
   }
 
-  const app = new Application(  {
+  function loadOverWorld(app: Application) {
+    const viewport = new FancyViewport({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+      worldWidth: WORLD_WIDTH,
+      worldHeight: WORLD_HEIGHT,
+      interaction: app.renderer.plugins.interaction,
+    });
+
+    app.stage.addChild(viewport);
+    viewport.moveCenter(0,0);
+
+    const toolbar = new Toolbar(viewport, app.view);
+    app.stage.addChild(toolbar);
+    toolbar.position.set(0, window.innerHeight - toolbar.height);
+
+    window.addEventListener("resize", () => {
+      toolbar.position.set(0, window.innerHeight - toolbar.height);
+    });
+
+    viewport.viewPort.on("clicked", (d: ClickEventData) => {
+      const {x, y} = d.screen;
+      for (let i = 0; i < musicianBios.length; i++) {
+        const b = musicianBios[i].getBounds(true);
+        // Basic rectable*point collision
+        if(x < b.x) continue;
+        if(x > b.x + b.width) continue;
+        if(y < b.y) continue;
+        if(y > b.y + b.height) continue;
+        return; // Cannot unfocus all, we've collided with a bioelement
+      }
+      unFocusAll(musicianBios);
+    });
+
+    const bioContainer = new Container();
+    bioContainer.sortableChildren = true;
+    viewport.addChild(bioContainer);
+
+    const musicianBios: Array<BioElement> = BIO_DATA.map((m) => new BioElement(m));
+    const activeInstruments: Array<Instrument> = [];
+    musicianBios.forEach((b) => {
+      b.on("focused", () => {
+        unFocusAll(musicianBios, b);
+        setTimeout(() => {
+          const bounds = b.getLocalBounds();
+          const panx = bounds.x + bounds.width / 2 + b.position.x;
+          const pany = bounds.y + bounds.height / 2 + b.position.y;
+          viewport.transitionCenter(panx, pany, 500);
+        }, 100);
+      });
+      b.on("activated", () => {
+        // Activate other bios with same instrument
+        if(activeInstruments.indexOf(b.musician.instrument) === -1) {
+          activeInstruments.push(b.musician.instrument);
+          const toActivate: Array<{bioElement: BioElement, distanceSquared: number}> = musicianBios
+            .filter((bf) => bf.musician.instrument === b.musician.instrument && bf !== b)
+            .map((bf) => {
+              return {
+                bioElement: bf,
+                distanceSquared: Math.pow(b.position.x - bf.position.x, 2) + Math.pow(b.position.y - bf.position.y, 2),
+              }
+            });
+
+          instrumentSoundActivate(b.musician.instrument);
+
+          const maxDistance = toActivate.reduce((p, {distanceSquared}) => Math.max(p, distanceSquared), 0);
+
+          for (let i = 0; i < toActivate.length; i++) {
+            const time = (toActivate[i].distanceSquared / maxDistance) * 3000;
+            setTimeout(toActivate[i].bioElement.activate.bind(toActivate[i].bioElement), time);
+          }
+        }
+      });
+    });
+
+    for (let i = 0; i < musicianBios.length; i++) {
+      const e = musicianBios[i];
+      bioContainer.addChild(e);
+      const x = (i % 5) * 200;
+      const y = Math.floor(i / 5) * 200;5
+      e.position.set(x, y);
+    }
+
+    function unFocusAll(allBios: Array<BioElement>, except?: BioElement) {
+      for (let i = 0; i < allBios.length; i++) {
+        if(except === undefined || allBios[i] !== except) {
+          allBios[i].unfocus();
+        }
+      }
+    }
+
+
+    // let i = 0;
+    // for (let r = 1; i < musicianBios.length; r++) {
+    //   for (let t = 0; t < Math.PI * 2 &&  i < musicianBios.length; t+=Math.PI / 7) {
+    //     const e = musicianBios[i];
+    //     bioContainer.addChild(e);
+    //     const dis = r * 250 - 50 + Math.random() * 100;
+    //     const tRand = Math.random() * Math.PI / 14
+    //     e.position.set(Math.cos(t + tRand) * dis, Math.sin(t + tRand) * dis);
+    //     i++;
+    //   }
+    // }
+    // Main loop
+    // app.ticker.add(() => {
+
+    // });
+  }
+
+  const app = new Application({
     antialias: true,
     transparent: false,
     autoDensity: true,
@@ -109,109 +218,9 @@ window.onload = async () => {
     backgroundColor: 0xC4C4C4,
   });
 
-  const viewport = new FancyViewport({
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerHeight,
-    worldWidth: WORLD_WIDTH,
-    worldHeight: WORLD_HEIGHT,
-    interaction: app.renderer.plugins.interaction,
-  });
-
   document.body.appendChild(app.view);
 
-  app.stage.addChild(viewport);
-  viewport.moveCenter(0,0);
-
-  const toolbar = new Toolbar(viewport, app.view);
-  app.stage.addChild(toolbar);
-  toolbar.position.set(0, window.innerHeight - toolbar.height);
-
-  window.addEventListener("resize", () => {
-    toolbar.position.set(0, window.innerHeight - toolbar.height);
-  });
-
-  const nucleus = new NucleusElement();
-  viewport.addChild(nucleus);
-
-  function unFocusAll(allBios: Array<BioElement>, except?: BioElement) {
-    for (let i = 0; i < allBios.length; i++) {
-      if(except === undefined || allBios[i] !== except) {
-        allBios[i].unfocus();
-      }
-    }
-  }
-
-  viewport.viewPort.on("clicked", (d: ClickEventData) => {
-    const {x, y} = d.screen;
-    for (let i = 0; i < musicianBios.length; i++) {
-      const b = musicianBios[i].getBounds(true);
-      // Basic rectable*point collision
-      if(x < b.x) continue;
-      if(x > b.x + b.width) continue;
-      if(y < b.y) continue;
-      if(y > b.y + b.height) continue;
-      return; // Cannot unfocus all, we've collided with a bioelement
-    }
-    unFocusAll(musicianBios);
-  });
-
-  const bioContainer = new Container();
-  bioContainer.sortableChildren = true;
-  viewport.addChild(bioContainer);
-
-  const musicianBios: Array<BioElement> = BIO_DATA.map((m) => new BioElement(m));
-  const activeInstruments: Array<Instrument> = [];
-  musicianBios.forEach((b) => {
-    b.on("focused", () => {
-      unFocusAll(musicianBios, b);
-      setTimeout(() => {
-        const bounds = b.getLocalBounds();
-        console.log(bounds);
-        const panx = bounds.x + bounds.width / 2 + b.position.x;
-        const pany = bounds.y + bounds.height / 2 + b.position.y;
-        viewport.transitionCenter(panx, pany, 500);
-      }, 100);
-    });
-    b.on("activated", () => {
-      // Activate other bios with same instrument
-      if(activeInstruments.indexOf(b.musician.instrument) === -1) {
-        activeInstruments.push(b.musician.instrument);
-        const toActivate: Array<{bioElement: BioElement, distanceSquared: number}> = musicianBios
-          .filter((bf) => bf.musician.instrument === b.musician.instrument && bf !== b)
-          .map((bf) => {
-            return {
-              bioElement: bf,
-              distanceSquared: Math.pow(b.position.x - bf.position.x, 2) + Math.pow(b.position.y - bf.position.y, 2),
-            }
-          });
-
-        instrumentSoundActivate(b.musician.instrument);
-
-        const maxDistance = toActivate.reduce((p, {distanceSquared}) => Math.max(p, distanceSquared), 0);
-
-        for (let i = 0; i < toActivate.length; i++) {
-          const time = (toActivate[i].distanceSquared / maxDistance) * 3000;
-          setTimeout(toActivate[i].bioElement.activate.bind(toActivate[i].bioElement), time);
-        }
-      }
-    });
-  });
-
-  let i = 0;
-  for (let r = 1; i < musicianBios.length; r++) {
-    for (let t = 0; t < Math.PI * 2 &&  i < musicianBios.length; t+=Math.PI / 7) {
-      const e = musicianBios[i];
-      bioContainer.addChild(e);
-      const dis = r * 250 - 50 + Math.random() * 100;
-      const tRand = Math.random() * Math.PI / 14
-      e.position.set(Math.cos(t + tRand) * dis, Math.sin(t + tRand) * dis);
-      i++;
-    }
-  }
-  // Main loop
-  // app.ticker.add(() => {
-
-  // });
+  loadOverWorld(app);
 
   function animate(time: number) {
     requestAnimationFrame(animate);
