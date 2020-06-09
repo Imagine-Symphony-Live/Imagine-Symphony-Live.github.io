46567bc93a2307c44e36eaa4a6aced424221bf6d
Dustin Woods <pyrozzcom@gmail.com>
2020-06-08 18:23:53 -0700

    improve draw arc

--- src/draw-util.ts
+++ src/draw-util.ts
@@ -1,27 +1,27 @@
 import { Graphics } from "pixi.js";
 
-export function drawDoubleClosedArc(graphics: Graphics, startArc: number, endArc: number, startRadius: number, endRadius: number, steps: number = 100, overlapEnd = false) {
+export function drawDoubleClosedArc(graphics: Graphics, startArc: number, endArc: number, startRadius: number, endRadius: number, steps: number = 100, overlapEnd = false, yScale = 1) {
 
   const stepArc = (endArc - startArc) / steps;
 
-  graphics.moveTo(Math.cos(startArc) * startRadius, -Math.sin(startArc) * startRadius);
+  graphics.moveTo(Math.cos(startArc) * startRadius, -Math.sin(startArc) * startRadius * yScale);
 
   for(let i = startArc + stepArc; i <= endArc - stepArc; i += stepArc) {
-    graphics.lineTo(Math.cos(i) * startRadius, -Math.sin(i) * startRadius);
+    graphics.lineTo(Math.cos(i) * startRadius, -Math.sin(i) * startRadius * yScale);
   }
 
-  graphics.lineTo(Math.cos(endArc) * startRadius, -Math.sin(endArc) * startRadius);
-  graphics.lineTo(Math.cos(endArc) * endRadius, -Math.sin(endArc) * endRadius);
+  graphics.lineTo(Math.cos(endArc) * startRadius, -Math.sin(endArc) * startRadius * yScale);
+  graphics.lineTo(Math.cos(endArc) * endRadius, -Math.sin(endArc) * endRadius * yScale);
 
   for(let i = endArc - stepArc; i >= startArc + stepArc; i -= stepArc) {
-    graphics.lineTo(Math.cos(i) * endRadius, -Math.sin(i) * endRadius);
+    graphics.lineTo(Math.cos(i) * endRadius, -Math.sin(i) * endRadius * yScale);
   }
 
-  graphics.lineTo(Math.cos(startArc) * endRadius, -Math.sin(startArc) * endRadius);
-  graphics.lineTo(Math.cos(startArc) * startRadius, -Math.sin(startArc) * startRadius);
+  graphics.lineTo(Math.cos(startArc) * endRadius, -Math.sin(startArc) * endRadius * yScale);
+  graphics.lineTo(Math.cos(startArc) * startRadius, -Math.sin(startArc) * startRadius * yScale);
 
   if(overlapEnd) {
-    graphics.lineTo(Math.cos(startArc + stepArc) * startRadius, -Math.sin(startArc + stepArc) * startRadius);
+    graphics.lineTo(Math.cos(startArc + stepArc) * startRadius, -Math.sin(startArc + stepArc) * startRadius * yScale);
   }
 
 }
